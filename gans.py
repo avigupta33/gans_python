6b104cdb32e44eed7400713d3f542d80ee071e2c
Avi Gupta <avigupta7@gmail.com>
2020-06-16 15:09:04 -0700

    Added display, zeros Added main

---
+++ gans.py
@@ -0,0 +1,6 @@
+from matrix import Matrix
+
+if __name__ == "__main__":
+
+    mat = Matrix.zeros(4,6)
+    mat.display()
--- matrix.py
+++ matrix.py
@@ -7,8 +7,21 @@ class Matrix:
         self.rows = len(data)
         self.cols = len(data[0])
 
-    def get(self, row: int, col:int ) -> int:
-        return self.data[row][col]
+    def __repr__(self):
+        return f"Matrix with {self.rows} rows and {self.cols} cols"
 
-    def set(self, row: int, col: int, value: int) -> None:
-        self.data[row][col] = value
+    def __getitem__(self, key) -> int:
+        return getattr(self.data, key)
+
+    def __setitem__(self, key) -> int:
+        return setattr(self.data, key)
+
+    def display(self) -> None:
+        for row in self.data:
+            for val in row:
+                print(val, end = " ")
+            print()
+
+    @classmethod
+    def zeros(cls, rows, cols):
+        return cls([[0 for _ in range(cols)] for _ in range(rows)])
