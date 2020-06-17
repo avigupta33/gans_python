ea5f3a6537e68bacb63f68c10a441ce0eb34f7f9
Avi Gupta <avigupta7@gmail.com>
2020-06-17 16:21:36 -0700

    Minor progress on optimizer

--- optimizer.py
+++ optimizer.py
@@ -6,14 +6,22 @@ from typing import *
 """
 class AdamOptimizer:
     def __init__(self, step_size = 0.001,
-                 b1 = 0.9, b2 = 0.99, delta = 10 ** (-8),
+                 b1 = 0.9, b2 = 0.99, epsilon = 10 ** (-8),
                  minibatch_size = 16):
         self.step_size = step_size
-        self.p1 = p1
-        self.p2 = p2
-        self.delta = delta
+        self.b1 = b1
+        self.b2 = b2
+        self.epsilon = epsilon
 
-    def optimize(self, params, training_set):
+    def optimize(self, params, training_set, objective_func):
+        m0 = 0
+        v0 = 0
+        t = 0
         while True: #TODO: Figure out stopping condition
+            t+=1
+
+
+
+
 
 
