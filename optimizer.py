eb5b391c9cc9f1950e937ab154a296d0329c8f29
Avi Gupta <avigupta7@gmail.com>
2020-06-17 16:30:41 -0700

    Initial implementation of adam

--- optimizer.py
+++ optimizer.py
@@ -1,24 +1,34 @@
 from matrix import Matrix
 from typing import *
+import math
 """
  D. P. Kingma and J. Ba, "Adam: A Method for Stochastic Optimization," 
  CoRR, vol. abs/1412.6980, 2014. [Online]. Available: A Method for Stochastic Optimization
 """
 class AdamOptimizer:
-    def __init__(self, step_size = 0.001,
-                 b1 = 0.9, b2 = 0.99, epsilon = 10 ** (-8),
-                 minibatch_size = 16):
-        self.step_size = step_size
+    def __init__(self, params, alpha = 0.001,
+                 b1 = 0.9, b2 = 0.99, eps = 10 ** (-8)):
+        self.params = params
+        self.alpha = alpha
         self.b1 = b1
         self.b2 = b2
-        self.epsilon = epsilon
+        self.eps = eps
+        self.m = 0
+        self.v = 0
+        self.t = 0
 
-    def optimize(self, params, training_set, objective_func):
-        m0 = 0
-        v0 = 0
-        t = 0
-        while True: #TODO: Figure out stopping condition
-            t+=1
+    def back_pass(self, grad):
+        self.t+=1
+        self.m = self.b1 * self.m + (1 - self.b1) * grad
+        self.v = self.b2 * self.m + (1 - self.b2) * (grad ** 2)
+        m_hat = self.m/(1-self.b1)
+        v_hat = self.v/(1-self.b2)
+        self.params -= self.alpha * m_hat/(math.sqrt(v_hat) - self.eps)
+        return self.params
+
+
+
+
 
 
 
