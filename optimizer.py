eaa4b4803ec7a4fc6493bfeb9054a114429d5429
Avi Gupta <avigupta7@gmail.com>
2020-06-17 16:42:47 -0700

    Added type hinting

--- optimizer.py
+++ optimizer.py
@@ -1,13 +1,16 @@
 from matrix import Matrix
+import typing
 from typing import *
+Vector = Matrix # 1D matrices only
+
 import math
 """
  D. P. Kingma and J. Ba, "Adam: A Method for Stochastic Optimization," 
  CoRR, vol. abs/1412.6980, 2014. [Online]. Available: A Method for Stochastic Optimization
 """
 class AdamOptimizer:
-    def __init__(self, params, alpha = 0.001,
-                 b1 = 0.9, b2 = 0.99, eps = 10 ** (-8)):
+    def __init__(self, params: Matrix, alpha: float= 0.001,
+                 b1:float = 0.9, b2: float = 0.99, eps: float = 10 ** (-8)) -> None:
         self.params = params
         self.alpha = alpha
         self.b1 = b1
@@ -17,7 +20,7 @@ class AdamOptimizer:
         self.v = 0
         self.t = 0
 
-    def back_pass(self, grad):
+    def back_pass(self, grad: Vector ) -> Matrix:
         self.t+=1
         self.m = self.b1 * self.m + (1 - self.b1) * grad
         self.v = self.b2 * self.m + (1 - self.b2) * (grad ** 2)
