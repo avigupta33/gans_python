bd0783c4bda41313ee5ccf1a14147ab828eebe88
Avi Gupta <avigupta7@gmail.com>
2020-06-17 17:39:07 -0700

    Removed testing code

--- gans.py
+++ gans.py
@@ -3,6 +3,3 @@ from loss_functions import binary_cross_entropy_loss
 
 if __name__ == "__main__":
     print("Hello, World!")
-    test = Matrix(97,1, [x/100 for x in range(1,98)])
-    test2 = Matrix(97,1,[x/100 for x in range(2,99)])
-    print(binary_cross_entropy_loss(test, test2))
