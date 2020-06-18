95db13d05435755496963c6a81b170fb82fab357
Avi Gupta <avigupta7@gmail.com>
2020-06-17 17:41:30 -0700

    Fixed typo

--- loss_functions.py
+++ loss_functions.py
@@ -3,5 +3,5 @@ from matrix import Matrix
 Vector = Matrix
 
 def binary_cross_entropy_loss(labels: Vector, preds: Vector) -> float:
-    return -1/len(preds) * sum(y * math.log(p) + (1-y) + math.log(1-p)
+    return -1/len(preds) * sum(y * math.log(p) + (1-y) * math.log(1-p)
                                for y,p in zip(labels.data, preds.data))
